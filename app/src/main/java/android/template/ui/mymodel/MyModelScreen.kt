6685fb70a73de6e7eacec2093968c574188f76f0
shubham08gupta <shubham.bestfriendforu@gmail.com>
2024-01-09 17:42:52 +0100

    Show UI for all UI states

--- app/src/main/java/android/template/ui/mymodel/MyModelScreen.kt
+++ app/src/main/java/android/template/ui/mymodel/MyModelScreen.kt
@@ -20,21 +20,29 @@ import android.template.data.local.database.Movie
 import android.template.ui.theme.MyApplicationTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -49,18 +57,43 @@ fun MyModelScreen(
     onAddNewMovie: () -> Unit,
     onMovieClicked: (name: Movie) -> Unit
 ) {
-    Scaffold(floatingActionButton = {
-        FloatingActionButton(onClick = { onAddNewMovie() }) {
-            Icon(Icons.Filled.Add, "Floating action button.")
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { onAddNewMovie() }) {
+                Icon(Icons.Filled.Add, "Floating action button.")
+            }
+        },
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
         }
-    }) { paddingValues ->
+    ) { paddingValues ->
         val items by viewModel.uiState.collectAsStateWithLifecycle()
-        if (items is MyModelUiState.Success) {
-            MyModelScreen(
-                movies = (items as MyModelUiState.Success).data,
-                onMovieClicked = { onMovieClicked(it) },
-                modifier = modifier.padding(paddingValues)
-            )
+        when (items) {
+            is MyModelUiState.Error -> {
+                LaunchedEffect(snackbarHostState) {
+                    snackbarHostState.showSnackbar(
+                        (items as MyModelUiState.Error).throwable.message ?: "Some error occurred"
+                    )
+                }
+            }
+
+            MyModelUiState.Loading -> {
+                CircularProgressIndicator(
+                    Modifier
+                        .fillMaxSize()
+                        .wrapContentSize(Alignment.Center)
+                )
+            }
+
+            is MyModelUiState.Success -> {
+                MyModelScreen(
+                    movies = (items as MyModelUiState.Success).data,
+                    onMovieClicked = { onMovieClicked(it) },
+                    modifier = modifier.padding(paddingValues)
+                )
+            }
         }
     }
 }
